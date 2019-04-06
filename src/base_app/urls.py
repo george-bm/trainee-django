b93d0c85d2bc2680e013e89231446ae0b2fbcd22
George <gbogomolov@gmail.com>
2019-04-06 17:58:23 +0200

    add speechtotext view

--- src/base_app/urls.py
+++ src/base_app/urls.py
@@ -4,4 +4,5 @@ from base_app import views
 
 urlpatterns = [
     url('health', views.HeartBeatHealthCheck.as_view(), name='common_healthcheck'),
+    url('speechtotext', views.SpeechToText.as_view(), name='common_speechtotext'),
 ]
--- src/base_app/views/__init__.py
+++ src/base_app/views/__init__.py
@@ -1,5 +1,7 @@
 from .healthcheck import HeartBeatHealthCheck
+from .speechtotext import SpeechToText
 
 __all__ = [
     'HeartBeatHealthCheck',
+    'SpeechToText',
 ]
